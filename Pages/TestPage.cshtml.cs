7fa44a29fa596afee4fefc4feb10e8a76b44a7bf
Eugene Zaznoba <irishjimdegriz@gmail.com>
2024-09-28 17:44:38 +0300

    Added changes for staging

---
+++ Pages/TestPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_zev.Pages
+{
+    public class TestPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
