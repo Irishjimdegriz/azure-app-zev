a60e4d7a1eb121ffdd8e8032cd2230f3db0e206e
Eugene Zaznoba <irishjimdegriz@gmail.com>
2024-09-28 13:25:54 +0300

    Config test

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace azure_app_zev.Pages;
@@ -6,14 +5,16 @@ namespace azure_app_zev.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _config;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
     }
 
     public void OnGet()
     {
-
+        ViewData["Greeting"] = _config["Greeting"];
     }
 }
